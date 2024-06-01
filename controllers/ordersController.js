ff717ec065e78e51864d38fd1e711f2381ccb435
gurisondh2002 <gurisondh2002@gmail.com>
2024-06-01 20:07:59 +0530

    Added

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -21,45 +21,45 @@ module.exports = {
             const { userId, productId, address, delivery_status, payment_method } = req.body;
 
 
-            try{
-                const params = {
-                    submit_type : 'pay',
-                    mode : "payment",
-                    payment_method_types : ['card'],
-                    billing_address_collection : "auto",
-                    shipping_options : [{shipping_rate : "shr_1PMovQ04h1WM8e3abkbkhsGj"}],
+            // try{
+            //     const params = {
+            //         submit_type : 'pay',
+            //         mode : "payment",
+            //         payment_method_types : ['card'],
+            //         billing_address_collection : "auto",
+            //         shipping_options : [{shipping_rate : "shr_1PMovQ04h1WM8e3abkbkhsGj"}],
           
-                    line_items : req.body.map((item)=>{
-                      return{
-                        price_data : {
-                          currency : "inr",
-                          product_data : {
-                            name : item.title,
-                            // images : [item.image]
-                          },
-                          unit_amount : item.price * 100,
-                        },
-                        adjustable_quantity : {
-                          enabled : true,
-                          minimum : 1,
-                        },
-                        quantity : item.qty
-                      }
-                    }),
+            //         line_items : req.body.map((item)=>{
+            //           return{
+            //             price_data : {
+            //               currency : "inr",
+            //               product_data : {
+            //                 name : item.title,
+            //                 // images : [item.image]
+            //               },
+            //               unit_amount : item.price * 100,
+            //             },
+            //             adjustable_quantity : {
+            //               enabled : true,
+            //               minimum : 1,
+            //             },
+            //             quantity : item.qty
+            //           }
+            //         }),
           
-                    success_url : `${process.env.FRONTEND_URL}/success`,
-                    cancel_url : `${process.env.FRONTEND_URL}/cancel`,
+            //         success_url : `${process.env.FRONTEND_URL}/success`,
+            //         cancel_url : `${process.env.FRONTEND_URL}/cancel`,
           
-                }
+            //     }
           
                 
-                const session = await stripe.checkout.sessions.create(params)
-                // console.log(session)
-                res.status(200).json(session.id)
-               }
-               catch (err){
-                  res.status(err.statusCode || 500).json(err.message)
-               }
+            //     const session = await stripe.checkout.sessions.create(params)
+            //     // console.log(session)
+            //     res.status(200).json(session.id)
+            //    }
+            //    catch (err){
+            //       res.status(err.statusCode || 500).json(err.message)
+            //    }
 
             let payment_status;
             if (payment_method === 'COD') {
